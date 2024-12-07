3457777f55a50883bf6e36de2dbe2d156d42a33e
Rafi Siddique <rafisiddique67@gmail.com>
2024-12-07 23:57:42 +0600

    fixed footer issue

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           About us
         </Link>
         <Link className="link link-hover">Contact</Link>
-        <Link className="link link-hover" to="/allCampaigns">
+        <Link className="link link-hover" to="/campaigns">
           All Campaign
         </Link>
       </nav>

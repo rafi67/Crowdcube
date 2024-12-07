7842042d7d3aa2d429479ab4dca71716231f7fc4
Rafi Siddique <rafisiddique67@gmail.com>
2024-12-07 22:05:57 +0600

    auth provider added

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { createContext } from "react";
+import { app } from "../firebase/firebase_init";
+import PropTypes from "prop-types";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth/cordova";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const provider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userLogin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const userLoginWithLogin = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    signOut(auth);
+  };
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  const updateUserProfile = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  const authInfo = {
+    user,
+    loading,
+    setUser,
+    createUser,
+    userLogin,
+    userLoginWithLogin,
+    logOut,
+    resetPassword,
+    updateUserProfile,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.element,
+};
+
+export default AuthProvider;

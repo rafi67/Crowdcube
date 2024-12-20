4d23ca4ee2a523d2091d79d3e1efdcfb3c016191
Rafi Siddique <rafisiddique67@gmail.com>
2024-12-20 09:53:25 +0600

    sorting challenge part done

--- src/Pages/AllCampaigns.jsx
+++ src/Pages/AllCampaigns.jsx
@@ -1,32 +1,64 @@
-import { useLoaderData } from "react-router-dom";
-import AllCampaignTable from "../Components/AllCampaignTable";
+import { Link, useLoaderData } from "react-router-dom";
+import { useState } from "react";
 
 const AllCampaigns = () => {
-  const campaign = useLoaderData();
+  const data = useLoaderData();
+  const [campaign, setCampaign] = useState(data);
+
+  const handleSort = () => {
+    setCampaign([...campaign].sort((a, b) => a.amount - b.amount));
+  };
 
   return (
-     <div className="overflow-x-auto">
-          <table className="table table-zebra">
-            {/* head */}
-            <thead>
+    <div className="overflow-x-auto">
+      <table className="table table-zebra">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Campaign Image</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Campaign Title</th>
+            <th>Campaign Type</th>
+            <th>Minimum Donation Amount</th>
+            <th>Deadline</th>
+            <th>Details</th>
+            <th>
+              <button className="btn btn-neutral" onClick={handleSort}>
+                Sort
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {campaign.map((data) => (
+            <>
               <tr>
-                <th>Campaign Image</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Campaign Title</th>
-                <th>Campaign Type</th>
-                <th>Minimum Donation Amount</th>
-                <th>Deadline</th>
-                <th>Details</th>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img src={data.photo} alt="Avatar Tailwind CSS Component" />
+                    </div>
+                  </div>
+                </td>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.title}</td>
+                <td>{data.type}</td>
+                <td>{data.amount}</td>
+                <td>{data.deadLine}</td>
+                <td>{data.description}</td>
+                <td>
+                  <Link className="btn" to={`/details/${data._id}`}>
+                    See More
+                  </Link>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {campaign.map(data => (
-                <AllCampaignTable key={data._id} user={data}></AllCampaignTable>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
